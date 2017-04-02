44efc1420f9436e447cc31f09663c9f20fb46dee
Cyril Ferlicot <c.ferlicot-delbecque@etudiant.univ-lille1.fr>
2017-04-02 18:45:20 +0200

    Personal site almost done

--- work/Mi -  Ultimate Personal Resume vCard template v1_3/assets/js/core.js
+++ work/Mi -  Ultimate Personal Resume vCard template v1_3/assets/js/core.js
@@ -263,6 +263,60 @@ var Mi = {
                 var pickedStyle = $googleMap.data('style');     
                 var dark = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}];
                 var light = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
+                var perso=[
+                    {
+                        "featureType": "water",
+                        "elementType": "geometry",
+                        "stylers": [
+                            { "color": "#808080" },
+                            { "lightness": -32 }
+                        ]
+                    },{
+                        "featureType": "landscape",
+                        "stylers": [
+                            { "color": "#808080" }
+                        ]
+                    },{
+                        "featureType": "poi",
+                        "elementType": "geometry",
+                        "stylers": [
+                            { "color": "#808080" },
+                            { "lightness": -14 }
+                        ]
+                    },{
+                        "featureType": "administrative",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                            { "color": "#808080" },
+                            { "lightness": 32 }
+                        ]
+                    },{
+                        "featureType": "road",
+                        "elementType": "geometry.fill",
+                        "stylers": [
+                            { "lightness": 11 },
+                            { "color": "#e69b24" }
+                        ]
+                    },{
+                        "featureType": "road",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                            { "color": "#ebbc63" },
+                            { "lightness": 11 }
+                        ]
+                    },{elementType:"labels.icon",stylers:[{visibility:"off"}]},{
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                            { "saturation": -100 }
+                        ]
+                    },{
+                        "featureType": "transit",
+                        "elementType": "geometry",
+                        "stylers": [
+                            { "saturation": -100 }
+                        ]
+                    }
+                ];
 
                 var pickedStyle = $googleMap.data('style');   
                 var myOptions = {
